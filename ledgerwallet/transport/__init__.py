a3acb2fe95d69cea50eec9006ecbb3ac3ff45279
Jean-Baptiste Bédrune <jean-baptiste.bedrune@ledger.fr>
2019-10-29 16:41:53 +0100

    Enable registration of transport classes

--- ledgerwallet/transport/__init__.py
+++ ledgerwallet/transport/__init__.py
@@ -1,9 +1,11 @@
 from .hid import HidDevice
 from .tcp import TcpDevice
 
+DEVICE_CLASSES = [TcpDevice, HidDevice]
+
 
 def enumerate_devices():
     devices = []
-    for cls in [TcpDevice, HidDevice]:
+    for cls in DEVICE_CLASSES:
         devices.extend(cls.enumerate_devices())
     return devices
