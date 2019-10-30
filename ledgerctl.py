c262ee7b74cf218b8c9bd181790205388ae0422f
Jean-Baptiste Bédrune <jean-baptiste.bedrune@ledger.fr>
2019-10-30 11:10:28 +0100

    Simple device key management + rename connect()

--- ledgerctl.py
+++ ledgerctl.py
@@ -24,6 +24,33 @@ def remote_options(func):
     return func
 
 
+def get_private_key() -> bytes:
+    app_path = click.get_app_dir("ledgerctl")
+    if not os.path.exists(app_path):
+        os.mkdir(app_path)
+
+    cfg_file = os.path.join(app_path, "config.ini")
+    try:
+        config = configparser.RawConfigParser()
+        config.read(cfg_file)
+
+        default_config = config['DEFAULT']
+        private_key = bytes.fromhex(default_config['private_key'])
+    except KeyError:
+        private_key = PrivateKey()
+        public_key = private_key.pubkey
+        pubkey_bytes = public_key.serialize(compressed=False)
+
+        config = configparser.RawConfigParser()
+        config['DEFAULT'] = {
+            "public_key": pubkey_bytes.hex(),
+            "private_key": private_key.serialize().hex()
+        }
+        with click.open_file(cfg_file, "w") as f:
+            config.write(f)
+    return private_key
+
+
 @click.group()
 @click.option("-v", "--verbose", is_flag=True, help="Display exchanged APDU.")
 @click.pass_context
@@ -32,29 +59,19 @@ def cli(ctx, verbose):
         utils.enable_apdu_log()
 
     def get_client():
-        app_path = click.get_app_dir("ledgerctl")
-        try:
-            cfg_file = os.path.join(app_path, "config.ini")
-            config = configparser.RawConfigParser()
-            config.read(cfg_file)
-
-            default_config = config['DEFAULT']
-            private_key = bytes.fromhex(default_config['private_key'])
-        except KeyError:
-            private_key = bytes.fromhex("5244aa214e6190220583754f80eb80e9d2b04a94c2c551e882f5c37a97077ab2")
         devices = enumerate_devices()
         if len(devices) == 0:
             click.echo("No Ledger device has been found.")
             sys.exit(0)
-        return LedgerClient(devices[0], private_key=private_key)
+        return LedgerClient(devices[0], private_key=get_private_key())
     ctx.obj = get_client
 
 
 @cli.command(help="Send raw data to the device.")
 @click.argument("input_file", type=click.File('r'))
 @click.pass_obj
-def send(connect, input_file):
-    client = connect()
+def send(get_client, input_file):
+    client = get_client()
     while True:
         chunk = input_file.readline()
         if not chunk:
@@ -66,8 +83,8 @@ def send(connect, input_file):
 @cli.command(help="Check if device is genuine.")
 @remote_options
 @click.pass_obj
-def genuine_check(connect, url, key):
-    if connect().genuine_check(url, key):
+def genuine_check(get_client, url, key):
+    if get_client().genuine_check(url, key):
         click.echo("Device is genuine.")
     else:
         click.echo("Device is NOT genuine.")
@@ -77,8 +94,8 @@ def genuine_check(connect, url, key):
 @click.option('--remote', default=False, is_flag=True)
 @remote_options
 @click.pass_obj
-def list_apps(connect, remote, url, key):
-    client = connect()
+def list_apps(get_client, remote, url, key):
+    client = get_client()
     rows = []
     for app in client.list_apps_remote(url, key) if remote else client.apps:
         rows.append([app.name, utils.flags_to_string(app.flags), app.code_data_hash.hex(), app.full_hash.hex()])
@@ -92,8 +109,8 @@ def list_apps(connect, remote, url, key):
 @click.argument("manifest")
 @click.option("-f", "--force", help="Delete using application hash instead of application name", is_flag=True)
 @click.pass_obj
-def install_app(connect, manifest: AppManifest, force):
-    client = connect()
+def install_app(get_client, manifest: AppManifest, force):
+    client = get_client()
     app_manifest = AppManifest(manifest)
     try:
         if force:
@@ -115,8 +132,8 @@ def install_app(connect, manifest: AppManifest, force):
 @click.argument("key_path")
 @remote_options
 @click.pass_obj
-def install_remote_app(connect, app_path, key_path, url, key):
-    client = connect()
+def install_remote_app(get_client, app_path, key_path, url, key):
+    client = get_client()
     client.install_remote_app(app_path, key_path, url, key)
 
 
@@ -124,13 +141,13 @@ def install_remote_app(connect, app_path, key_path, url, key):
 @click.argument("app")
 @click.option("--by-hash", help="Delete using application hash instead of application name", is_flag=True)
 @click.pass_obj
-def delete_app(connect, app, by_hash):
+def delete_app(get_client, app, by_hash):
     if by_hash:
         data = bytes.fromhex(app)
     else:
         data = app
     try:
-        connect().delete_app(data)
+        get_client().delete_app(data)
     except CommException as e:
         if e.sw == 0x6985:
             click.echo("Operation has been canceled by the user.")
@@ -143,17 +160,17 @@ def delete_app(connect, app, by_hash):
 @click.argument("key_path")
 @remote_options
 @click.pass_obj
-def delete_app_remote(connect, app_path, key_path, url, key):
-    client = connect()
+def delete_app_remote(get_client, app_path, key_path, url, key):
+    client = get_client()
     client.delete_remote_app(app_path, key_path, url, key)
 
 
 @cli.command(help="Run specified application.")
 @click.argument("app_name")
 @click.pass_obj
-def run(connect, app_name: str):
+def run(get_client, app_name: str):
     try:
-        connect().run_app(app_name)
+        get_client().run_app(app_name)
     except CommException as e:
         if e.sw == 0x6984:
             click.echo("Application {} is not installed.".format(app_name))
@@ -165,31 +182,15 @@ def run(connect, app_name: str):
 @click.argument("name")
 @click.argument("public_key", required=False)
 @click.pass_obj
-def install_ca(connect, name, public_key):
+def install_ca(get_client, name, public_key):
     if public_key is None:
-        click.echo("Generating certificate authority...")
-
-        private_key = PrivateKey()
-        public_key = private_key.pubkey
-        pubkey_bytes = public_key.serialize(compressed=False)
-
-        app_path = click.get_app_dir("ledgerctl")
-        if not os.path.exists(app_path):
-            os.mkdir(app_path)
-
-        cfg_file = os.path.join(app_path, "config.ini")
-        config = configparser.RawConfigParser()
-        config['DEFAULT'] = {
-            "public_key": pubkey_bytes.hex(),
-            "private_key": private_key.serialize().hex()
-        }
-        with click.open_file(cfg_file, "w") as f:
-            config.write(f)
+        raw_private_key = get_private_key()
+        pubkey_bytes = PrivateKey(raw_private_key).pubkey.serialize(compressed=False)
     else:
         pubkey_bytes = bytes.fromhex(public_key)
 
     try:
-        connect().install_ca(name, pubkey_bytes)
+        get_client().install_ca(name, pubkey_bytes)
         click.echo("Custom certificate has been successfully installed.")
     except CommException as e:
         if e.sw == 0x6982:
@@ -202,9 +203,9 @@ def install_ca(connect, name, public_key):
 
 @cli.command(help="Delete custom certificate authority.")
 @click.pass_obj
-def delete_ca(connect):
+def delete_ca(get_client):
     try:
-        connect().delete_ca()
+        get_client().delete_ca()
         click.echo("Custom certificate has been deleted.")
     except CommException as e:
         if e.sw == 0x6a84:
@@ -224,11 +225,11 @@ def sizeof_fmt(num, suffix='B'):
 
 @cli.command(help="Display device memory usage.")
 @click.pass_obj
-def meminfo(connect):
+def meminfo(get_client):
     def format_entry(name: str, size: int, max_size: int):
         return "- {0:s}: {1:s} ({2:.2f}%)".format(name, sizeof_fmt(size), size / max_size * 100)
 
-    memory_info = connect().get_memory_info()
+    memory_info = get_client().get_memory_info()
 
     total_size = memory_info.system_size + memory_info.applications_size + memory_info.free_size
     click.echo("Memory usage:")
@@ -242,8 +243,8 @@ def meminfo(connect):
 
 @cli.command(help="Display device information.")
 @click.pass_obj
-def info(connect):
-    version_info = connect().get_version_info_secure()
+def info(get_client):
+    version_info = get_client().get_version_info_secure()
 
     click.echo("Device: {} ({})".format(utils.get_device_name(version_info.target_id), version_info.target_id))
     click.echo("SE version: {}".format(version_info.se_version))
@@ -261,8 +262,8 @@ def info(connect):
 @click.argument("firmware_key")
 @remote_options
 @click.pass_obj
-def upgrade_firmware(connect, firmware_name, firmware_key, url, key):
-    client = connect()
+def upgrade_firmware(get_client, firmware_name, firmware_key, url, key):
+    client = get_client()
     client.upgrade_firmware(firmware_name, firmware_key, url, key)
 
 
