c4b58a6dc14229f359b252af8978d2ccea91dfa5
Lucas PASCAL <lucas.pascal@ledger.com>
2023-05-26 16:49:40 +0200

    [ci][add] test.pypi.org deployment on every merge on main branches, pypi.org deployment on tag

--- ledgerwallet/__init__.py
+++ ledgerwallet/__init__.py
@@ -1,3 +1,6 @@
 """Python client and library to communicate with Ledger devices."""
 
-__version__ = "0.2.4"
+try:
+    from ledgerwalet.__version__ import __version__  # noqa
+except ImportError:
+    __version__ = "unknown version"  # noqa
