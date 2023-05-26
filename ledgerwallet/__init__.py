55eb8ee67a36fd060e4165cb1cf13e319974dba8
Lucas PASCAL <lucas.pascal@ledger.com>
2023-05-26 17:43:57 +0200

    [ci][update] Moving from flit to twine as flit_scm seems not to be working properly

--- ledgerwallet/__init__.py
+++ ledgerwallet/__init__.py
@@ -1,6 +1,6 @@
 """Python client and library to communicate with Ledger devices."""
 
 try:
-    from ledgerwalet.__version__ import __version__  # noqa
+    from ledgerwallet.__version__ import __version__  # noqa
 except ImportError:
     __version__ = "unknown version"  # noqa
